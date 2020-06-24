09dabf838aafb094eb91cf0dcce5d0f5df32e44a
MohanedKassim <aliyyi89@gmail.com>
2020-06-24 13:18:59 -0500

    GET for talent

---
+++ src/redux/reducers/talentProfileReducer.js
@@ -0,0 +1,12 @@
+const talentProfileReducer = (state = [], action) =>{
+    if(action.type === 'SET_TALENT'){
+      console.log(action.payload);
+      return action.payload
+     
+    } else {
+      return state
+    }
+    
+  }
+  
+  export default talentProfileReducer;
