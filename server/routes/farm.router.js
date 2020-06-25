945ae3b6c6386caad20c70d917efa7be5d758cdb
Josh Kuss <joshua.d.kuss@gmail.com>
2020-06-25 14:45:30 -0500

    farm router has initial get post and put routes

--- server/routes/farm.router.js
+++ server/routes/farm.router.js
@@ -9,7 +9,7 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 router.get('/farm/:id', rejectUnauthenticated, (req, res) => {
     let id = req.user.id
     console.log('in farm router get', [id]);
-    const sqlText = `SELECT * FROM farm ORDER BY "id";`;
+    const sqlText = `SELECT * FROM farm ORDER BY "id";`; /// match to user id!
   pool
     .query(sqlText, [id])
     .then((result) => {
@@ -42,5 +42,25 @@ router.post('/', rejectUnauthenticated, (req, res) => {
         res.sendStatus(500);
     })
 });
+
+router.put('/:id', rejectUnauthenticated, (req, res) => {  // '/:id'
+     console.log('pg router put');
+    //console.log('router put', req.body);
+    const queryText = `UPDATE farm
+            SET "farm_name" = $2, "farm_address"= $3, "city" = $4, "state" = $5, "zipcode" = $6,
+            "phone" = $7, "size" = $8, "type" = $9, "bio" = $10
+            WHERE "id"=$1`;
+    const queryValues = [req.body.id, req.body.pg_name, req.body.description];
+    pool.query(queryText, queryValues)
+        .then(() => {
+            res.sendStatus(200);
+            console.log("router put worked!");
+        })
+        .catch((err) => {
+            console.log('Error updating pg, router put pg router', err);
+            res.sendStatus(500)
+        });
+});
+
 
 module.exports = router;
