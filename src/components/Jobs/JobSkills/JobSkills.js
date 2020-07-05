75700c669b2d7c4b817e9aad9552c75e487c6c49
Josh Kuss <joshua.d.kuss@gmail.com>
2020-07-05 13:56:13 -0500

    small update

--- src/components/Jobs/JobSkills/JobSkills.js
+++ src/components/Jobs/JobSkills/JobSkills.js
@@ -18,12 +18,9 @@ export class JobSkills extends Component {
                 <CardContent  >
                     <h4> General Agriculture </h4>
 
-<<<<<<< HEAD
                     {/* <ul> */}
                         <div>
-=======
                   
->>>>>>> 3157b393a1624cf7c2d291bd51ca01a3f8c923f2
                         {this.props.proficiencies.generalAgriculture.map((item) => {
                             return (
                                 <JobSkillsItem item={item} key={item.id} />
@@ -32,11 +29,8 @@ export class JobSkills extends Component {
                         })}
 
 
-<<<<<<< HEAD
                     </div>
                     {/* </ul> */}
-=======
->>>>>>> 3157b393a1624cf7c2d291bd51ca01a3f8c923f2
 
                     <h4> Precision Farming Technology  </h4>
 
