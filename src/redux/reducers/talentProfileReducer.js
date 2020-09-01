c422ef56265b99c82a39394db437736750e9dc6b
Josh Kuss <joshua.d.kuss@gmail.com>
2020-09-01 13:45:23 -0500

    Update talentProfileReducer.js

--- src/redux/reducers/talentProfileReducer.js
+++ src/redux/reducers/talentProfileReducer.js
@@ -2,11 +2,9 @@ const talentProfileReducer = (state = [], action) =>{
     if(action.type === 'SET_TALENT'){
       console.log(action.payload);
       return action.payload
-     
     } else {
       return state
-    }
-    
+    }  
   }
   
   export default talentProfileReducer;
